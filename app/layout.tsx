5b3126882d07ff62eb8130601996540c3c70eb44
nethmesaboth <nethmesaboth10@gmail.com>
2024-07-24 15:43:24 +0700

    init the project with nextjs

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  title: "LiveDocs",
+  description: "Visual documentation for your codebase",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
